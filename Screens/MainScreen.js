e629f3d584566b5cbcecac20b0d78fc6e0163980
CarlosZelayaTec <79062915+CarlosZelayaTec@users.noreply.github.com>
2022-02-02 21:17:19 -0600

    Empezamos y utilizaremos virtualizedList

--- Screens/MainScreen.js
+++ Screens/MainScreen.js
@@ -1,46 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
+import { StyleSheet, Text, View, VirtualizedList, Image } from 'react-native';
 import ListaProductos from '../Components/ListaProductos';
 import ImagesProducts from '../Api/ImagesProducts';
 
 const MainScreen = ({ navigation }) => {
 
-    const idCategoria = navigation.getParam('idCategoria');
-
-    const [productos, setProductos] = useState([])
-    const [imagenes, setImagenes] = useState([]);
-
-    const fetchProductos = () => {
-        const data = require('../Api/Productos.json');
-        setImagenes(ImagesProducts);
-        setProductos(data);
-    }
-
-    
-    useEffect(() => {
-        fetchProductos();
-    })
-
     return(
-        <View style={styles.container} >
-            <FlatList 
-                data={productos.filter(x => x.categoriaId === idCategoria)}
-                keyExtractor={x => x.id}
-                numColumns={2}
-                renderItem={({item}) => <ListaProductos idP={item.id} ttt={item.nameProduc} precio={item.precio} imagenes={imagenes}/>}
-            />
+        <View>
+            <Text>Estamos en la rama de optimizado</Text>
+            <Text>Aqui utilizaremos VirutalizedList</Text>
         </View>
     )
+
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        // alignSelf: 'stretch',
-        // justifyContent: 'center',
-        // alignItems: 'center',
         backgroundColor: '#202641',
-        // backgroundColor: '#FFFFFF',
     }
 })
 
